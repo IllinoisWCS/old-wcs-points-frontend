1a3883fca42af8b3488a9c05b81d139937af9a68
Alice Fang <alicesf2@illinois.edu>
2018-10-03 14:53:09 -0500

    got rid of this space for rent

--- source/components/dash/dash.jsx
+++ source/components/dash/dash.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
-
-import styles from './dash.scss'
-
-class Dash extends Component {
-    render() {
-        return(
-            <div className="Dash">
-                <h1>WCS Events Log</h1>
-                <br />
-                <p>This space for rent.</p>
-            </div>
-        )
-    }
-}
-
-export default Dash
+// import React, { Component } from 'react'
+//
+// import styles from './dash.scss'
+//
+// class Dash extends Component {
+//     render() {
+//         return(
+//             <div className="Dash">
+//                 <h1>WCS Events Log</h1>
+//                 <br />
+//                 <p>This space for rent.</p>
+//             </div>
+//         )
+//     }
+// }
+// 
+// export default Dash
--- source/components/home/home.jsx
+++ source/components/home/home.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 import styles from './home.scss'
 
-import Dash from '../dash/dash.jsx'
 import Events from '../events/events.jsx'
 import Points from '../points/points.jsx'
 import SignIn from '../signin/signin.jsx'
@@ -17,17 +16,15 @@ class Home extends Component {
                     <div className="Home__leftNav">
                         <img src="./assets/logo.png" />
                         <div className="Home__flex">
-                            <Link className="Home__flexItem" to="/">Home</Link>
-                            <Link className="Home__flexItem" to="/events">Events</Link>
+                            <Link className="Home__flexItem" to="/">Events</Link>
                             <Link className="Home__flexItem" to="/points">Points</Link>
                             <Link className="Home__flexItem" to="/signin">Sign-in</Link>
                         </div>
                     </div>
                     <div className="Home__content">
                         <div>
-                            <Route exact path="/" component={Dash}/>
+                            <Route exact path="/" component={Events}/>
                             <Route exact path="/points" component={Points}/>
-                            <Route exact path="/events" component={Events}/>
                             <Route exact path="/signin" component={SignIn}/>
                         </div>
                     </div>
