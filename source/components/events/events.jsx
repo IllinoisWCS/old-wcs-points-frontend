8a72b43a9dada5cc2b85c66044dd9be7f190303c
Alice Fang <alicesf2@illinois.edu>
2019-02-08 21:45:23 -0600

    removed netids from events

--- source/components/events/events.jsx
+++ source/components/events/events.jsx
@@ -26,10 +26,10 @@ class Events extends Component {
       events.sort(function(a, b) {
         var dateA = new Date(a.date).getTime();
         var dateB = new Date(b.date).getTime();
-        if (dateA < dateB) {
+        if (dateA > dateB) {
           return -1;
         }
-        if (dateA > dateB) {
+        if (dateA < dateB) {
           return 1;
         }
 
@@ -45,46 +45,175 @@ class Events extends Component {
 
   render() {
     const events = this.state.events.map(event => {
-      // const attendees = event.attendees.map( (attendee) => {
-      //     return(<Label>{attendee}</Label>)
-      // })
+      if (event.category === "Corporate") {
+        return (
+          <Segment className="Events__corp" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
 
-      const panels = [
-        {
-          title: "Show Attendees",
-          content: {
-            content: (
-              <Segment className="Events__attendees">{attendees}</Segment>
-            )
-          }
-        }
-      ];
-
-      return (
-        <Segment className="Events__event" padded>
-          <div className="Events__flex">
-            <div className="Events__flexItem">
-              <h3>{event.name}</h3>
-
-              <h5 className="muted">
-                {new Date(event.date).toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric"
-                })}
-              </h5>
-
-              <Accordion panels={panels} />
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
             </div>
-            <div className="Events__flexItem">
-              <Statistic className="Events_statistic" size="tiny">
-                <Statistic.Value>{event.attendees.length}</Statistic.Value>
-                <Statistic.Label>Attended</Statistic.Label>
-              </Statistic>
+          </Segment>
+        );
+      } else if (event.category === "General Meeting") {
+        return (
+          <Segment className="Events__gm" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
+
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
             </div>
-          </div>
-        </Segment>
-      );
+          </Segment>
+        );
+      } else if (event.category === "Tech Team") {
+        return (
+          <Segment className="Events__techTeam" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
+
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
+            </div>
+          </Segment>
+        );
+      } else if (event.category === "Social") {
+        return (
+          <Segment className="Events__social" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
+
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
+            </div>
+          </Segment>
+        );
+      } else if (event.category === "Mentoring") {
+        return (
+          <Segment className="Events__mentoring" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
+
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
+            </div>
+          </Segment>
+        );
+      } else if (event.category === "Outreach") {
+        return (
+          <Segment className="Events__outreach" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
+
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
+            </div>
+          </Segment>
+        );
+      } else {
+        return (
+          <Segment className="Events__event" padded>
+            <div className="Events__flex">
+              <div className="Events__flexItem">
+                <h3>{event.name}</h3>
+
+                <h5 className="muted">
+                  {new Date(event.date).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric"
+                  })}
+                </h5>
+              </div>
+              <div className="Events__flexItem">
+                <Statistic className="Events_statistic" size="tiny">
+                  <Statistic.Value>{event.attendees.length}</Statistic.Value>
+                  <Statistic.Label>Attended</Statistic.Label>
+                </Statistic>
+              </div>
+            </div>
+          </Segment>
+        );
+      }
     });
 
     return (
