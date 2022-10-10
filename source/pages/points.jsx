7c1fee01ee4aff264f77462eb2aa66f39b606497
gracewzhang <gracewzhang01@gmail.com>
2022-10-10 16:55:31 -0500

    Convert points

--- source/pages/points.jsx
+++ source/pages/points.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Input, Button, Segment, Card, Message } from "semantic-ui-react";
 import Notifications, { notify } from "react-notify-toast";
 
@@ -8,46 +8,29 @@ const utils = require("../utils");
 
 import "../styles/points.scss";
 
-class Points extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: "",
-      events: [],
-      totalPoints: null,
-      error: false,
-    };
+const Points = () => {
+  const [value, setValue] = useState("");
+  const [events, setEvents] = useState([]);
+  const [totalPoints, setTotalPoints] = useState(null);
+  const [error, setError] = useState(false);
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleEnter = this.handleEnter.bind(this);
-  }
-
-  handleSubmit() {
-    if (this.state.value !== "") {
-      if (!this.state.value.match("^[A-Za-z0-9]*$")) {
-        this.setState({
-          error: true,
-          totalPoints: null,
-          events: [],
-        });
+  const handleSubmit = () => {
+    if (value !== "") {
+      if (!value.match("^[A-Za-z0-9]*$")) {
+        setError(true);
+        setTotalPoints(null);
+        setEvents([]);
         return;
       } else {
-        this.setState({
-          error: false,
-        });
+        setError(false);
       }
-
       axios
         .get(
-          "http://points-api.illinoiswcs.org/api/users/" +
-            this.state.value.toLowerCase()
+          "http://points-api.illinoiswcs.org/api/users/" + value.toLowerCase()
         )
         .then((response) => {
           // axios.get('http://localhost:3000/api/users/' + this.state.value).then( (response) => {
-          this.setState({
-            totalPoints: response.data.result?.points,
-          });
+          setTotalPoints(response.data.result?.points);
 
           if (response.data.result != null) {
             const eventKeys = response.data.result.attendedEvents;
@@ -59,33 +42,29 @@ class Points extends Component {
                 },
               })
               .then((response) => {
-                this.setState({
-                  events: response.data.result,
-                });
+                setEvents(response.data.result);
               });
           } else {
-            this.setState({
-              events: [],
-              totalPoints: 0,
-            });
+            setEvents([]);
+            setTotalPoints(0);
           }
         });
     } else {
       notify.show("Please enter a net id", "error");
     }
-  }
+  };
 
-  handleEnter(tgt) {
+  const handleEnter = (tgt) => {
     if (tgt.charCode === 13) {
-      this.handleSubmit();
+      handleSubmit();
     }
-  }
+  };
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-  _renderAttendedEvents(events) {
+  const _renderAttendedEvents = (events) => {
     return events.map((event, id) => (
       <Segment className="event-detail" padded key={id}>
         <div>
@@ -98,48 +77,46 @@ class Points extends Component {
         </div>
       </Segment>
     ));
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <h1>Points</h1>
-        <Card fluid className="Points">
-          <Card.Content>
-            <br />
-            <Input
-              fluid
-              icon="search"
-              placeholder="Enter your NetID ..."
-              value={this.state.value}
-              onChange={this.handleChange}
-              onKeyPress={this.handleEnter}
+  return (
+    <div>
+      <h1>Points</h1>
+      <Card fluid className="Points">
+        <Card.Content>
+          <br />
+          <Input
+            fluid
+            icon="search"
+            placeholder="Enter your NetID ..."
+            value={value}
+            onChange={handleChange}
+            onKeyPress={handleEnter}
+          />
+          <br />
+          <Button onClick={handleSubmit} fluid>
+            Check Points
+          </Button>
+          {error && (
+            <Message
+              error
+              header="Invalid Net ID"
+              content="Net ID can only contain numbers and letters."
             />
-            <br />
-            <Button onClick={this.handleSubmit} fluid>
-              Check Points
-            </Button>
-            {this.state.error && (
-              <Message
-                error
-                header="Invalid Net ID"
-                content="Net ID can only contain numbers and letters."
-              />
+          )}
+          <br />
+          <div className="points-message">
+            {totalPoints != null && (
+              <h1>{`You have ${totalPoints} total points.`}</h1>
             )}
-            <br />
-            <div className="points-message">
-              {this.state.totalPoints != null && (
-                <h1>{`You have ${this.state.totalPoints} total points.`}</h1>
-              )}
-            </div>
-            {this._renderAttendedEvents(this.state.events)}
-          </Card.Content>
-        </Card>
+          </div>
+          {_renderAttendedEvents(events)}
+        </Card.Content>
+      </Card>
 
-        <Notifications />
-      </div>
-    );
-  }
-}
+      <Notifications />
+    </div>
+  );
+};
 
 export default Points;
